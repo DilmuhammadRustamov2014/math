1f03c9a87abfb93106427c19fa62a0887eddced8
DilmuhammadRustamov2014 <dilmuhammad.itf@gmail.com>
2025-01-05 18:31:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log(Math.floor(16.9))
+
+    var num = Number(prompt("Birinchi sonni kiriting"));
+    var num1 = Number(prompt("ikkinchi sonni kiriting"));
+    const operation = +prompt("1) + \n 2) - \n 3) * \n 4) /")
+    
+    if(operation == 1){
+        alert(num + num1);
+    }else if(operation == 2)
+
+    alert(num - num1);
+    if(operation == 3
+    ){
+        alert(num * num1);
+    }else if(operation == 4){
+    alert(num / num1);
+    }
+    const secretNumber = Math.floor(Math.random() * 10) + 1;
+
